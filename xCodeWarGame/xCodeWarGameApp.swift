bad69c026c5046ae233ed2d435ae5cf6c5130ba9
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-02-17 08:32:09 -0600

    Initial Commit

---
+++ xCodeWarGame/xCodeWarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  xCodeWarGameApp.swift
+//  xCodeWarGame
+//
+//  Created by Jack Hyde on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct xCodeWarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
