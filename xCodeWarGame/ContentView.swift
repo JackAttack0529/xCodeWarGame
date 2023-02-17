6a7e85152dcaaba6cb64205fde6f9a5d1cde32d5
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-02-17 09:01:37 -0600

    Created all content/elements for game.

--- xCodeWarGame/ContentView.swift
+++ xCodeWarGame/ContentView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
+            Image("background")
+            
+            VStack {
+                Image("logo")
+                
+                HStack{
+                    Image("card5")
+                    Image("card5")
+                }
+                
+                Button {
+                    print("Button clicked!")
+                } label: {
+                    Image("dealbutton")
+                }
+                
+                HStack {
+                    VStack {
+                        Text("Player")
+                        Text("0")
+                    }
+                    VStack {
+                        Text("CPU")
+                        Text("0")
+                    }
+                }
+            }
         }
     }
 }
